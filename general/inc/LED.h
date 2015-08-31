97f5f29cd22631093b6bd61f161354ced7dd3164
Lars Jansen <lars@ljpc.nl>
2015-08-31 22:11:41 +0200

    Included LCD and comments

--- general/inc/LED.h
+++ general/inc/LED.h
@@ -1,4 +1,16 @@
-void LED_Init(void);
+#define    LED0       0x01
+#define    LED1       0x02
+#define    LED2       0x04
+#define    LED3       0x08
+#define    LED4       0x10
+#define    LED5       0x20
+#define    LED6       0x40
+#define    LED7       0x80
+
+#define    LED_ALL    LED0 | LED1 | LED2 | LED3 | LED4 | LED5 | LED6 | LED7
+#define    LED_NONE   0x00
+
+void LED_Init (void);
 void LED_On (unsigned int num);
 void LED_Off (unsigned int num);
-void LED_Out(unsigned int value);
+void LED_Out (unsigned int value);
--- program.c
+++ program.c
@@ -1,16 +1,48 @@
+/*
+ * This clean example program is made by Lars Jansen
+ * GitHub repository: https://github.com/Lars-/MCB2300-Clean-Example
+ */
+
+//System files
 #include <stdio.h>
 #include <LPC23xx.H>
+
+//User files
+#include "LCD.h"
 #include "LED.h"
 #include "timing.h"
 
 int main(void)
 {
-    LED_Init();
+    //Variables
+    unsigned int i = 0;	             //Start counter i at 0
+    char counter[7];                 //Make a char array to display the counter on the screen later
+
+    //Initialisation
+    lcd_init();                      //Initialise the LCD screen
+    lcd_clear();                     //Clear the LCD screen
+
+    LED_Init();                      //Initialise the LEDs
+
+    //Do forever
     while(1)
     {
-        LED_Out(0xFF);
-        delay(1000);
-        LED_Out(0x00);
-        delay(1000);
+        LED_Out(LED_ALL);            //Turn on all LEDs
+        delay(1000);                 //Wait one second
+
+        LED_Out(LED_NONE);           //Turn on none LEDs (so turn them all off)
+        delay(1000);                 //Wait one second
+
+        i++;                         //Count i
+        lcd_clear();                 //Clear the LCD
+        lcd_print("Counter: ");
+        sprintf(counter, "%07d", i); //Cast the counter to a 7 char array (lcd_print doesn't support ints)
+        lcd_print(counter);          //Print the array
+
+        //Little (and simple) protection for char array overflow
+        if(i == 9999999)
+        {
+            i = 0;
+        }
     }
 }
